bfcaaf61744c6a63f45bbd32c79f96b052172e9a
Gerd Friemel <gerd.friemel@gmail.com>
2018-03-25 19:08:32 +0200

    git: branches to be deleted are saved in a registry.

--- clients/sync_client.py
+++ clients/sync_client.py
@@ -1,4 +1,5 @@
 import os
+from git import Repo
 
 from . import ACTION_PULL, ACTION_PUSH, ACTION_SET_CONF, ACTION_SET_CONF_ALIASES, ACTION_DELETE
 
@@ -23,19 +24,42 @@ class SyncClientFactory:
             print('unison')
             return None
 
-
 class DeletionRegistration:
+
     def __init__(self, path):
         self.path = path
+        clients_dir = os.path.dirname(os.path.realpath(__file__))
+        self.registry_dir = os.path.dirname(clients_dir) + '/var'
 
     def get_mode(self):
         # first check if directory is a git working tree
-        current_dir = os.getcwd()
-        if os.path.isdir(current_dir + '/.git'):
+        self.dir = os.getcwd()
+        if os.path.isdir(self.dir + '/.git'):
+            # first check if this branch exists
+            gitrepo = Repo(self.dir)
+            if not hasattr(gitrepo.heads, self.path):
+                return None
             return 'git'
-        #to be implemented: Unison check for
+        #to be implemented: Unison check
         return None
 
     def register_path(self):
         mode = self.get_mode()
-        pass
+        if not mode:
+            return None
+        registry_file = self.registry_dir + '/' + mode + '.txt'
+        if mode == 'git':
+            f = open(registry_file,'+a')
+            entry = self.dir + '\t' + self.path
+            f.write(entry)
+            f.close()
+
+    def read_and_flush_registry(self, mode):
+        registry_file = self.registry_dir + '/' + mode + '.txt'
+        if mode == 'git':
+            f = open(registry_file,'+a')
+            entries = f.readlines()
+            f.seek(0)
+            f.truncate()
+            f.close()
+            return entries
