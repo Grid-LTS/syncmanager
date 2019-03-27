a642f67b9947c63160033e0f9e0f02085e2066eb
Gerd Friemel <gerd.friemel@gmail.com>
2019-03-27 17:00:41 +0100

    #IMPLEMENT api: create admin blueprint and create user endpoint.

---
+++ syncmanagerapi/syncmanagerapi/admin/controller.py
@@ -0,0 +1,23 @@
+from flask import request, Response, json
+from . import admin
+
+
+@admin.route('/user', methods=['POST'])
+def parse_request():
+    from ..error import InvalidRequest
+    from ..utils import generate_password
+    from ..model import User, Roles
+
+    body = request.data
+    if not body:
+        raise InvalidRequest('Empty body', 'username')
+    data = request.get_json(force=True)
+    username = data['username']
+    if not data.get('password', None):
+        password = generate_password()
+    else:
+        password = data['password']
+    User.add_user(_username=username, _password=password, _role=Roles.DEFAULT)
+    resp_data = dict()
+    resp_data['password'] = password
+    return Response(response=json.dumps(resp_data), status=200, mimetype='application/json')
---
+++ syncmanagerapi/syncmanagerapi/utils.py
@@ -0,0 +1,7 @@
+import string
+import secrets
+
+def generate_password(length=12):
+    pwchars = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(pwchars) for i in range(length))
+    return password
