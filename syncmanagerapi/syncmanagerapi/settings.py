05b36ec916cdc7bb43ecffd7103796f741ee97d5
Gerd Friemel <gerd.friemel@gmail.com>
2019-05-19 10:52:13 +0200

    [api] provide DB connection config to Prod environment.

--- syncmanagerapi/syncmanagerapi/settings.py
+++ syncmanagerapi/syncmanagerapi/settings.py
@@ -6,6 +6,7 @@ import configparser
 top_package_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(top_package_dir)
 
+
 def import_entry_point(function_name):
     try:
         import syncmanagerapi
@@ -14,17 +15,24 @@ def import_entry_point(function_name):
         import syncmanagerapi
     return getattr(syncmanagerapi, function_name)
 
+
 def read_properties_file():
-    with open(os.path.join(project_dir, 'application.properties')) as propertiesfile:
+    # first determine environment
+    if os.environ.get('SYNCMANAGER_SERVER_CONF', None):
+        properties_dir = os.environ['SYNCMANAGER_SERVER_CONF']
+    else:
+        # DEV environment
+        install_properties_path = os.path.join(project_dir, 'application.properties')
+        install_propertiesfile = open(install_properties_path, 'r')
+        install_config = configparser.ConfigParser()
+        install_config.optionxform = str
+        config_string = '[default_section]\n' + install_propertiesfile.read()
+        install_config.read_string(config_string)
+        properties_dir = os.path.join(install_config['default_section'].get('INSTALL_DIR'), 'conf')
+    properties_path = os.path.join(properties_dir, 'application.properties')
+    with open(properties_path) as propertiesfile:
         config = configparser.ConfigParser()
         config.optionxform = str
         config_string = '[default_section]\n' + propertiesfile.read()
         config.read_string(config_string)
-        # read installation dependent properties
-        instance_properties_path = config['default_section'].get('INSTALL_DIR')
-        instance_propertiesfile = open(os.path.join(instance_properties_path,'conf/vars.conf'), 'r')
-        instanceconfig = configparser.ConfigParser()
-        instanceconfig.optionxform = str
-        config_string = '[default_section]\n' + instance_propertiesfile.read()
-        instanceconfig.read_string(config_string)
-        return {**config['default_section'], **instanceconfig['default_section'] }
+        return {**config['default_section']}
