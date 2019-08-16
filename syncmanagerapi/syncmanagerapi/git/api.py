df91df05ed74e057aeb2f7573d98a78098197b6f
Gerd Friemel <gerd.friemel@gmail.com>
2019-08-16 09:21:35 +0200

    #IMPLEMENT [api]: associate Git repo to user on creation, GET endpoint for list of user's git repos.

--- syncmanagerapi/syncmanagerapi/git/api.py
+++ syncmanagerapi/syncmanagerapi/git/api.py
@@ -3,18 +3,33 @@ from ..error import InvalidRequest
 
 
 def create_repo():
-    from .model import GitRepo
+    from .model import GitRepo, GitRepoSchema
+    from ..model import User
     body = request.data
     from ..decorators import requires_auth
     requires_auth()
     auth = request.authorization
     if not body:
         raise InvalidRequest('Empty body', 'repo_name')
+    user = User.user_by_username(auth['username'])
     data = request.get_json(force=True)
     repo_name = data['repo_name']
     if data.get('server_path_relative', None):
         server_path_rel = data['server_path_relative']
     else:
         server_path_rel = f"{repo_name}.git"
-    GitRepo.add_git_repo(_repo_name=repo_name, _server_path_rel=server_path_rel)
-    return Response(status=204)
+    gitrepo = GitRepo.add_git_repo(_repo_name=repo_name, _server_path_rel=server_path_rel, _user_id=user.id,
+                                   _local_path_rel="home/code", _client_id="home")
+    gitrepo_schema = GitRepoSchema()
+    return gitrepo_schema.dump(gitrepo).data
+
+
+def get_repos():
+    from .model import UserGitReposAssocSchema
+    from ..model import User
+    from ..decorators import requires_auth
+    requires_auth()
+    auth = request.authorization
+    user = User.user_by_username(auth['username'])
+    user_gitrepo_assoc_schema = UserGitReposAssocSchema(many=True)
+    return user_gitrepo_assoc_schema.dump(user.gitrepos).data
--- syncmanagerapi/syncmanagerapi/git/model.py
+++ syncmanagerapi/syncmanagerapi/git/model.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import uuid
 
 from ..database import db, ma
+from ..model import User
 
 
 class GitRepo(db.Model):
@@ -17,11 +18,16 @@ class GitRepo(db.Model):
         return GitRepo.query.filter_by(id=_id)
 
     @staticmethod
-    def add_git_repo(_repo_name, _server_path_rel):
+    def add_git_repo(_repo_name, _server_path_rel, _user_id, _local_path_rel, _client_id='default'):
         _id = uuid.uuid4()
+        _assoc_id = uuid.uuid4()
         new_git = GitRepo(id=_id, server_path_rel=_server_path_rel, repo_name=_repo_name)
+        user_gitrepo_assoc = UserGitReposAssoc(id=_assoc_id, user_id=_user_id, local_path_rel=_local_path_rel,
+                                               client_id=_client_id)
+        new_git.users.append(user_gitrepo_assoc)
         db.session.add(new_git)
         db.session.commit()
+        return new_git
 
     def __repr__(self):
         return '<GitRepo %r>' % self.repo_id
@@ -33,12 +39,37 @@ class GitRepoSchema(ma.ModelSchema):
         sqla_session = db.session
 
 
-"""
-class UserGitRepos(db.Model):
+class UserGitReposAssoc(db.Model):
     __tablename__ = "user_git_repos"
+    id = db.Column(db.String(36), primary_key=True)
     user_id = db.Column(db.String(36), db.ForeignKey('user.id'))
     repo_id = db.Column(db.String(36), db.ForeignKey('git_repos.id'))
-    local_path_rel = db.Column(db.Text(), unique=True, nullable=False)
-    client_id = db.Column(db.String(100), unique=True, nullable=False) # identifies users machine
-    # people = Person.query.order_by(Person.lname).all()
-"""
+    local_path_rel = db.Column(db.Text(), nullable=False)
+    client_id = db.Column(db.String(100), nullable=False)  # identifies users machine
+    git_repo = db.relationship(GitRepo, backref="users")
+    user = db.relationship(User, backref="gitrepos")
+
+    @staticmethod
+    def add_user_gitrepo_assoc(_user_id, _repo_id, _local_path_rel, _client_id='default'):
+        """
+        sets an entry in the association table. This should only be used when associating an existing repository to 
+        another the user, given by id. When creating the repo in the database do not use this function, but create an
+        UserGitRepoAssoc object and pass it to the GitRepo dto before committing to the DB session
+        :param _user_id: id of the user
+        :param _repo_id: id of the git repo (should already be existing in the DB)
+        :param _local_path_rel: local path on the users machine
+        :param _client_id: name of the users machine
+        :return: 
+        """
+        _id = uuid.uuid4()
+        new_gitrep_assoc = UserGitReposAssoc(id=_id, user_id=_user_id, repo_id=_repo_id, local_path_rel=
+        _local_path_rel, client_id=_client_id)
+        db.session.add(new_gitrep_assoc)
+        db.session.commit()
+        return new_gitrep_assoc
+
+
+class UserGitReposAssocSchema(ma.ModelSchema):
+    class Meta:
+        model = UserGitReposAssoc
+        sqla_session = db.session
