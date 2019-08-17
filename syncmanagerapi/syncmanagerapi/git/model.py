407387e220f5e657f3a09eaad0b8d0fd323ca0c2
Gerd Friemel <gerd.friemel@gmail.com>
2019-08-17 12:10:10 +0200

    #IMPLEMENT [api]: creation of Git repository in server file system.

--- syncmanagerapi/syncmanagerapi/git/model.py
+++ syncmanagerapi/syncmanagerapi/git/model.py
@@ -1,33 +1,57 @@
 from datetime import datetime
+import os.path as osp
 import uuid
 
 from ..database import db, ma
 from ..model import User
+from marshmallow import fields
 
 
 class GitRepo(db.Model):
     __tablename__ = "git_repos"
     id = db.Column(db.String(36), primary_key=True)
-    repo_name = db.Column(db.String(100), nullable=False)
     server_path_rel = db.Column(db.Text(), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow())
     updated = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow())
+    user_id = None
+
+    def __init__(self, server_path_rel, user_id):
+        if server_path_rel[-4:] != '.git':
+            server_path_rel += '.git'
+        self.server_path_rel = osp.join(user_id, server_path_rel)
+        self.user_id = user_id
 
     @staticmethod
     def git_repo_by_id(_id):
         return GitRepo.query.filter_by(id=_id)
 
+    def add(self, _local_path_rel, _client_id='default'):
+        return GitRepo.persist_git_repo(self, _local_path_rel=_local_path_rel, _client_id=_client_id)
+
     @staticmethod
-    def add_git_repo(_repo_name, _server_path_rel, _user_id, _local_path_rel, _client_id='default'):
-        _id = uuid.uuid4()
-        _assoc_id = uuid.uuid4()
-        new_git = GitRepo(id=_id, server_path_rel=_server_path_rel, repo_name=_repo_name)
-        user_gitrepo_assoc = UserGitReposAssoc(id=_assoc_id, user_id=_user_id, local_path_rel=_local_path_rel,
-                                               client_id=_client_id)
-        new_git.users.append(user_gitrepo_assoc)
-        db.session.add(new_git)
-        db.session.commit()
-        return new_git
+    def add_git_repo(_server_path_rel, _user_id, _local_path_rel, _client_id='default'):
+        git_repo = GitRepo(server_path_rel=_server_path_rel, user_id=_user_id)
+        return GitRepo.persist_git_repo(git_repo, _local_path_rel=_local_path_rel, _client_id=_client_id)
+
+    @staticmethod
+    def persist_git_repo(git_repo_obj, _local_path_rel, _client_id='default'):
+        result = GitRepo.query.outerjoin(UserGitReposAssoc) \
+            .filter(GitRepo.server_path_rel == git_repo_obj.server_path_rel) \
+            .filter(UserGitReposAssoc.user_id == git_repo_obj.user_id) \
+            .one_or_none()
+        if not result:
+            _id = uuid.uuid4()
+            _assoc_id = uuid.uuid4()
+            git_repo_obj.id = _id
+            user_gitrepo_assoc = UserGitReposAssoc(id=_assoc_id, user_id=git_repo_obj.user_id,
+                                                   local_path_rel=_local_path_rel,
+                                                   client_id=_client_id)
+            git_repo_obj.userinfo.append(user_gitrepo_assoc)
+            db.session.add(git_repo_obj)
+            db.session.commit()
+        else:
+            git_repo_obj = result
+        return git_repo_obj
 
     def __repr__(self):
         return '<GitRepo %r>' % self.repo_id
@@ -46,7 +70,7 @@ class UserGitReposAssoc(db.Model):
     repo_id = db.Column(db.String(36), db.ForeignKey('git_repos.id'))
     local_path_rel = db.Column(db.Text(), nullable=False)
     client_id = db.Column(db.String(100), nullable=False)  # identifies users machine
-    git_repo = db.relationship(GitRepo, backref="users")
+    git_repo = db.relationship(GitRepo, backref="userinfo")
     user = db.relationship(User, backref="gitrepos")
 
     @staticmethod
@@ -62,8 +86,8 @@ class UserGitReposAssoc(db.Model):
         :return: 
         """
         _id = uuid.uuid4()
-        new_gitrep_assoc = UserGitReposAssoc(id=_id, user_id=_user_id, repo_id=_repo_id, local_path_rel=
-        _local_path_rel, client_id=_client_id)
+        new_gitrep_assoc = UserGitReposAssoc(id=_id, user_id=_user_id, repo_id=_repo_id,
+                                             local_path_rel=_local_path_rel, client_id=_client_id)
         db.session.add(new_gitrep_assoc)
         db.session.commit()
         return new_gitrep_assoc
@@ -73,3 +97,11 @@ class UserGitReposAssocSchema(ma.ModelSchema):
     class Meta:
         model = UserGitReposAssoc
         sqla_session = db.session
+
+
+class GitRepoFullSchema(ma.ModelSchema):
+    class Meta:
+        model = GitRepo
+        sqla_session = db.session
+
+    userinfo = fields.Nested(UserGitReposAssocSchema, default=[], many=True)
